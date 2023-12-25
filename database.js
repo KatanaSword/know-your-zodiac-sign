d78b8d48b4ef7601ba26d6b47a700dc0ec4376ca
KatanaSword <114555620+KatanaSword@users.noreply.github.com>
2023-12-25 14:52:31 +0530

    update database.js

---
+++ database.js
@@ -0,0 +1,100 @@
+const zodiacSigns = [
+  {
+    sign: "Aries",
+    startDate: "21/03",
+    endDate: "19/04",
+    zodiacElement: "Fire",
+    zodiacQuality: "Cardinal",
+    signRuler: "Mars"
+  },
+  {
+    sign: "Taurus",
+    startDate: "20/04",
+    endDate: "20/05",
+    zodiacElement: "Earth",
+    zodiacQuality: "Fixed",
+    signRuler: "Venus"
+  },
+  {
+    sign: "Gemini",
+    startDate: "21/05",
+    endDate: "20/06",
+    zodiacElement: "Air",
+    zodiacQuality: "Mutable",
+    signRuler: "Mercury"
+  },
+  {
+    sign: "Cancer",
+    startDate: "21/06",
+    endDate: "22/07",
+    zodiacElement: "Water",
+    zodiacQuality: "Cardinal",
+    signRuler: "Moon"
+  },
+  {
+    sign: "Leo",
+    startDate: "23/07",
+    endDate: "22/08",
+    zodiacElement: "Fire",
+    zodiacQuality: "Fixed",
+    signRuler: "Sun"
+  },
+  {
+    sign: "Virgo",
+    startDate: "23/08",
+    endDate: "22/09",
+    zodiacElement: "Earth",
+    zodiacQuality: "Mutable",
+    signRuler: "Mercury"
+  },
+  {
+    sign: "Libra",
+    startDate: "23/09",
+    endDate: "22/10",
+    zodiacElement: "Air",
+    zodiacQuality: "Cardinal",
+    signRuler: "Venus"
+  },
+  {
+    sign: "Scorpio",
+    startDate: "23/10",
+    endDate: "21/11",
+    zodiacElement: "Water",
+    zodiacQuality: "Fixed",
+    signRuler: "Pluto (traditional ruler: Mars)"
+  },
+  {
+    sign: "Sagittarius",
+    startDate: "22/11",
+    endDate: "21/12",
+    zodiacElement: "Fire",
+    zodiacQuality: "Mutable",
+    signRuler: "Jupiter"
+  },
+  {
+    sign: "Capricorn",
+    startDate: "22/12",
+    endDate: "19/01",
+    zodiacElement: "Earth",
+    zodiacQuality: "Cardinal",
+    signRuler: "Saturn"
+  },
+  {
+    sign: "Aquarius",
+    startDate: "20/01",
+    endDate: "18/02",
+    zodiacElement: "Air",
+    zodiacQuality: "Fixed",
+    signRuler: "Uranus (traditional ruler: Saturn)"
+  },
+  {
+    sign: "Pisces",
+    startDate: "19/02",
+    endDate: "20/03",
+    zodiacElement: "Water",
+    zodiacQuality: "Mutable",
+    signRuler: "Neptune (traditional ruler: Jupiter)"
+  },
+];
+
+export { zodiacSigns }
